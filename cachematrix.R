937f9753230d34dcac5f739857f6af432a14f22a
Michiel Borkent <michielborkent@gmail.com>
2014-05-06 09:54:37 +0200

    Improved comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,20 @@
-## Set of functions that implement caching of inverse matrix calculation
-## Use makeCacheMatrix to create a cached matrix
-## and call cacheSolve on it, to get cached results.
-## Example: 
-## m <-makeCacheMatrix(matrix(c(1,.5,0,.5),2,2))
-## cacheSolve(m) ## will compute the inverse, cache it and return it
-## cacheSolve(m) ## will return the cached result immediately
+# Set of functions that enable caching of inverse matrix calculation
+# Use makeCacheMatrix to create a cached matrix
+# and call cacheSolve on it, to get cached results.
+# 
+# Example session:
+#
+# > source('ProgrammingAssignment2/cachematrix.R')
+# > m <-makeCacheMatrix(matrix(c(1,.5,0,.5),2,2))
+# > cacheSolve(m)
+# [,1] [,2]
+# [1,]    1    0
+# [2,]   -1    2
+# > cacheSolve(m)
+# getting cached data
+# [,1] [,2]
+# [1,]    1    0
+# [2,]   -1    2
 
 ## Creates a matrix object with cachable inverse 
 makeCacheMatrix <- function(x = matrix()) {
@@ -21,11 +31,9 @@ makeCacheMatrix <- function(x = matrix()) {
        getInverse = getInverse)
 }
 
-
-## Calculates inverse of matrix object, 
-## or returns inverse if it has already been calculated
+## Returns calculates inverse of matrix object, 
+## or returns inverse immediately if it has already been calculated
 cacheSolve <- function(x, ...) {
-  ## Return a matrix that is the inverse of 'x'
   inv <- x$getInverse()
   if(!is.null(inv)) {
     message("getting cached data")
